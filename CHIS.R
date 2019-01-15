1487307c1e3f8405520fc884ff3154a727890b1a
David Ngai <NgaiTszTo@users.noreply.github.com>
2019-01-14 20:30:11 -0800

    Update CHIS.R

--- CHIS.R
+++ CHIS.R
@@ -40,3 +40,40 @@ ggplot(adult, aes (x = SRAGE_P, fill= RBMI)) +
   BMI_fill + 
   facet_grid(RBMI~.) +
   theme_classic()
+# Plot 1 - Count histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) +
+  geom_histogram(binwidth = 1) +
+  BMI_fill
+
+# Plot 2 - Density histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..density..), binwidth = 1) +
+  BMI_fill
+
+# Plot 3 - Faceted count histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(binwidth = 1) +
+  BMI_fill +
+  facet_grid(RBMI~.)
+
+
+
+# Plot 4 - Faceted density histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..density..), binwidth = 1) +
+  BMI_fill +
+  facet_grid(RBMI~.)
+
+
+
+# Plot 5 - Density histogram with position = "fill"
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..density..),position = "fill", binwidth = 1) +
+  BMI_fill
+
+
+
+# Plot 6 - The accurate histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..count../sum(..count..)),position = "fill", binwidth = 1) +
+  BMI_fill
