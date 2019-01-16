d3ac3a98d0d3714a18ef3175f50a144a0a3430f9
David Ngai <NgaiTszTo@users.noreply.github.com>
2019-01-15 16:34:42 -0800

    Update CHIS.R

--- CHIS.R
+++ CHIS.R
@@ -64,7 +64,30 @@ ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) +
   BMI_fill +
   facet_grid(RBMI~.)
 
+# Perform chi.sq test (RBMI and SRAGE_P)
+results <- chisq.test(table(adult$RBMI, adult$SRAGE_P))
 
+# Melt results$residuals and store as resid
+resid <- melt(results$residuals)
+
+# Change names of resid
+names(resid) <- c("FILL", "X", "residual")
+
+# merge the two datasets:
+DF_all <- merge(DF_melted, resid)
+
+# Update plot command
+library(ggthemes)
+ggplot(DF_all, aes(ymin = ymin,
+                   ymax = ymax,
+                   xmin = xmin,
+                   xmax = xmax,
+                   fill = residual)) +
+  geom_rect() +
+  scale_fill_gradient2() +
+  scale_x_continuous(expand = c(0,0)) +
+  scale_y_continuous(expand = c(0,0)) +
+  theme_tufte()
 
 # Plot 5 - Density histogram with position = "fill"
 ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
