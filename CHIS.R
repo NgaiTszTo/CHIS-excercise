270bf82900055a98dbb6d51ef33d0328c6f695ac
David Ngai <NgaiTszTo@users.noreply.github.com>
2019-01-16 00:12:30 -0800

    Update CHIS.R

--- CHIS.R
+++ CHIS.R
@@ -186,3 +186,24 @@ p1 %+% DF_all +
             y = 1, angle = 90,
             size = 3, hjust = 1,
             show.legend = FALSE)
+                 # Load all packages
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+library(ggthemes)
+
+# Script generalized into a function
+mosaicGG
+
+# BMI described by age (as previously seen)
+mosaicGG(adult, X = "SRAGE_P", FILL = "RBMI")
+
+# Poverty described by age
+mosaicGG(adult, X = "SRAGE_P", FILL = "POVLL" )
+
+# mtcars: am described by cyl
+mosaicGG(mtcars, X = "cyl", FILL ="am")
+
+# Vocab: vocabulary described by education
+library(carData)
+mosaicGG(Vocab, X = "education",FILL= "vocabulary")
